4acc978c631773ef7c01ecf66389500f90aa89d6
anwrat <anweshrwt22@gmail.com>
2024-12-08 21:11:10 +0545

    Baby steps lol

---
+++ firstcodes/letter.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Hullo myself\n";
+    std::cout<<"Date: 12/8/2024\n";
+    std::cout<<"Make sure to complete this course quick and grind leetcode lol\n";
+    return 0;
+}
