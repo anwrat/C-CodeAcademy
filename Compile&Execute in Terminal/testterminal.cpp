e2a9f6e05ac61a5e5792796e2abfb790d43a4704
anwrat <anweshrwt22@gmail.com>
2024-12-08 21:32:39 +0545

    Compiling using the terminal

---
+++ Compile&Execute in Terminal/testterminal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//For printing through terminal, go to the path in terminal and enter g++ filename.cpp
+//Then it creates a file such as a.exe, type ./a.exe  or the filename that is created
+
+int main(){
+    std::cout<<"Testing terminal output";
+    return 0;
+}
---
+++ firstcodes/commentsincpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    //std::cout also read as see-out prints output to terminal
+    std::cout<<"Hullo!";
+    /*std::cout<<"C++ is necessary?";
+    Just Joking*/
+    return 0;
+}
